c287c33226c7c6c05b6ae9c673ca3c74736f863f
AuracleDelphi <AuracleDelphi@users.noreply.github.com>
2022-05-20 15:17:49 -0400

    Code updates
    Added button press and loop pseudocode

--- 164D Arduino Code/src/main.cpp
+++ 164D Arduino Code/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h> 
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
 // setup
 // functions
@@ -8,21 +10,31 @@
 #define SOUND 10
 #define SOUNDGND 9
 #define BUTTON 8
+double refVoltage = 0;
+double measVoltage = 0;
+int bpm = 0;
+int t = 0;
+int temp = 0;
 
-
+// OLED Stuff
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the OLED name
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(BUTTON, INPUT);
   pinMode(SOUND,OUTPUT);
   pinMode(SOUNDGND, OUTPUT);
+  pinMode(BUTTON, INPUT);
   analogReference(INTERNAL); // Set ADC to 1.1V internal reference
 }
 
 void buzz(int dcycle, int bzzlen){
   // buzz the buzzer
   // dcycle between 0 and 1
-  int t = millis();
+  t = millis();
   while(millis() - t < bzzlen){
     analogWrite(SOUNDGND, 0);
     analogWrite(SOUND, dcycle);
@@ -46,6 +58,36 @@ double getADCVoltage(int pin){
   return voltage;
 }
 
+bool buttonPress(){
+  // Button pulled high, push brings low
+  if (digitalRead(BUTTON) == LOW){
+    return true;
+  }
+  return false;
+}
+
+void oledDisplay(String msg){
+  // I'll finish copying this over from class code soon
+}
+
+double getBPM(){
+  // I'll finish copying this over from class code soon
+  bpm = 0;
+  return bpm;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  if(buttonPress()){
+    //read ambient adc voltage
+    refVoltage = getADCVoltage(REF);
+    //read measurement adc voltage
+    measVoltage = getADCVoltage(MEAS);
+    //process these into temperatures
+    // TODO: SOME ACTUAL FORMULAS
+    temp = measVoltage * 10000004000
+    //display on oled
+    oledDisplay("Temp: " + String(temp))
+    //send over bluetooth
+  }
 }
