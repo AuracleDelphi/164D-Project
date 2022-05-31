0fb7951c1654c417d3f10adb30ec1a0e07d3349c
ericalazbin <ericalazbin@gmail.com>
2022-05-30 23:59:53 -0700

    Update BT_Test_Script.py

--- 164D Python Code/BT_Test_Script.py
+++ 164D Python Code/BT_Test_Script.py
@@ -13,4 +13,14 @@ while(True):
         print(retreiveData())
     else:
         ser.write(b'0')
+
+#sArr = ['']*40
+#for i in range(0,40):
+#    s = retreiveData()
+#    print(s)
+#    sArr[i] = s
+#ser.write(b'0')
+#print(sArr)
+
+
     
