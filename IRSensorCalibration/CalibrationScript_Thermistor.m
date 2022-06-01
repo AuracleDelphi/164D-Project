307ef48d3e4d980d6289964cf8d959096ec62dec
ericalazbin <ericalazbin@gmail.com>
2022-05-31 23:35:06 -0700

    Thermistor algorithm updated

--- IRSensorCalibration/CalibrationScript_Thermistor.m
+++ IRSensorCalibration/CalibrationScript_Thermistor.m
@@ -4,7 +4,7 @@ T_amb = table2array(T(:,3));
 T_amb = T_amb(1:end);
 voltage = table2array(T(:,2));
 voltage = voltage(1:end);
-f = fit(voltage,T_amb,"rat21")
+f = fit(voltage,T_amb,"rat22")
 plot(f,voltage, T_amb)
 xlabel("Voltage Reading (mV)")
 ylabel("Ambient Temperature (C)")
